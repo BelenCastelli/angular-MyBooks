400a8461392b8dc05818aa810da8f0c47f3d52b9
Belen Castelli <mariabelencastelli29@gmail.com>
2023-12-14 18:04:40 +0100

    binding

---
+++ src/app/models/user.ts
@@ -0,0 +1,28 @@
+export class User {
+    public id_user:number;
+    public name:string;
+    public last_name: string;
+    public email: string;
+    public photo: string;
+    public password: string
+
+    constructor(name:string, last_name:string, email:string, photo:string){
+        this.id_user 
+        this.name = name
+        this.last_name = last_name
+        this.email = email
+        this.photo = photo
+        this.password
+
+    }
+
+    nombreCompleto(){
+        return `${this.name} ${this.last_name}`; 
+    }
+
+    mostrarNombre(){
+        console.log(this.name);
+        
+    }
+
+}
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "src/app/models/user";
 
 @Component({
   selector: 'app-profile',
@@ -6,17 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
- public nombre:string;
- public apellido:string
- public correo:string;
- public foto: string;
+//  public nombre:string;
+//  public apellido:string
+//  public correo:string;
+//  public foto: string;
+
+ public user:User
+ public isHidden:boolean
+ public isHidden2: boolean
+ public color: string
+ 
 
 
   constructor(){
-    this.nombre = "Belén"
-    this.apellido = "Castelli Gubiotti";
-    this.correo = "belencastelli@gmail.com"
-    this.foto = "../../../assets/img-profile/avatar2.png"
- 
+    // this.nombre = "Belén"
+    // this.apellido = "Castelli Gubiotti";
+    // this.correo = "belencastelli@gmail.com"
+    // this.foto = "../../../assets/img-profile/avatar2.png"
+
+    this.user = new User("Belén", "Castelli Gubiotti", "belencastelli@gmail.com", "../../../assets/img-profile/avatar2.png")
+    this.isHidden = true;
+    this.isHidden2 = true
+    this.color;
   }
+  // ¿Por qué no ponerlo en la clase user?
+  modificarDatos(newName:string, newLastaName:string, newEmail:string,newPhoto:string){
+    console.log(this.user.name);
+    // ¿Se puede simplificar?
+
+    this.user.name = newName === "" ?  this.user.name : newName;
+
+    this.user.last_name = newLastaName == "" ? this.user.last_name : newLastaName;
+ 
+    this.user.email = newEmail == "" ? this.user.email : newEmail;
+  
+    this.user.photo = newPhoto == "" ? this.user.photo : newPhoto;
+
+    if(newName == "" && newLastaName == "" && newEmail == "" && newPhoto ==""){
+      this.isHidden2 = false;
+      this.isHidden = true;
+      this.color = "rojo"
+    } else
+    {
+      this.isHidden = false;
+      this.isHidden2 = true
+      this.color = "verde"
+    }
+
+
+  }
+
+
 }
